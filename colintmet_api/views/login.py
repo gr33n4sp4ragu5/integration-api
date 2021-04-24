fe96f78d38248211727e2babdc997303b4131698
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-04-24 19:13:54 +0200

    refactor(#1): Added utils to views folder and updated the path

--- colintmet_api/views/login.py
+++ colintmet_api/views/login.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from . import utils
+from colintmet_api import utils
 
 class Login(APIView):
     authentication_classes = (BasicAuthentication,)
