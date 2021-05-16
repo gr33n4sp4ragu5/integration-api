1bcb2d6e8119b93676aa3d009604cafef437c153
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-05-17 00:00:27 +0200

    fix(#3): I was overwriting the app directory with the volume that i was using to persist the sqlite3 databse

--- colintmet_api/settings.py
+++ colintmet_api/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'colintmet_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': str(BASE_DIR / 'sqlite/db.sqlite3'),
     }
 }
 
