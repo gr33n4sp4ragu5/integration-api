a5674afcc42b0119a460a08f6b9332ba238e442f
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-05-18 20:29:06 +0200

    feat(#3): serialize the information saved in the database and fixed serialization problem stringifying mongodb inserted id

--- colintmet_api/db_connector.py
+++ colintmet_api/db_connector.py
@@ -16,9 +16,15 @@ def establish_db_connection(url, database_name):
 def insert_new_user(db_connection, user):
     users_collection = db_connection['users']
     try:
-        inserted = users_collection.insert_one({"nome": "alucino"}).inserted_id
-        return inserted
+        inserted = users_collection.insert_one(serialize_user(user)).inserted_id
+        return str(inserted)
     except Exception as error:
         logging.error("Couldn't update database. Error:\n%s", error)
         raise Exception(f"""Error trying to insert user {user} and error is {error}""")
+
+
+def serialize_user(user):
+    return {'email': user['email'], 'gender': user['gender'],
+            'birthdate': user['birthdate'], 'name': user['name'],
+            'surnames': user['surnames']}
 
