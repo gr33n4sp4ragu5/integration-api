bc4138783482b931999f80112d03460dee2b9d97
Esther Hurtado Ruiz <esther@MBP-de-Esther.home>
2021-05-23 20:15:20 +0200

    feat(#5): Added datetime types to start and end date times

--- colintmet_api/db_connector.py
+++ colintmet_api/db_connector.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import logging
+import datetime
 
 
 def establish_db_connection(database_url, database_port, database_name):
@@ -42,7 +43,7 @@ def insert_survey_response(db_connection, survey_response, username):
 def serialize_survey_response(survey_response, username):
     question_ids = survey_response['survey']['results'].keys()
     results = [survey_response['survey']['results'][question_id]['results'] for question_id in question_ids]
-    start_date_time = survey_response['start_date']
-    end_date_time = survey_response['end_date']
+    start_date_time = datetime.datetime.strptime(survey_response['start_date'], '%Y-%m-%d %H:%M:%S.%f')
+    end_date_time = datetime.datetime.strptime(survey_response['end_date'], '%Y-%m-%d %H:%M:%S.%f')
     survey_id = survey_response['survey']['identifier']
     return {'user': username, 'results': results, 'id': survey_id, 'start_date_time': start_date_time, 'end_date_time': end_date_time}
