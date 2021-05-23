7b7722944b9bf468464b1a90901129d91e752c1c
Esther Hurtado Ruiz <esther@MBP-de-Esther.home>
2021-05-23 20:00:27 +0200

    feat(#5): improve formatting

--- colintmet_api/db_connector.py
+++ colintmet_api/db_connector.py
@@ -40,6 +40,8 @@ def insert_survey_response(db_connection, survey_response, username):
 
 
 def serialize_survey_response(survey_response, username):
+    question_ids = survey_response['survey']['results'].keys()
+    real_results = [survey_response['survey']['results'][question_id] for question_id in question_ids]
     results = survey_response['survey']['results']['questionStep0ID']['results']
     survey_id = survey_response['survey']['identifier']
-    return {'user': username, 'results': results, 'id': survey_id, 'survey_raw': survey_response}
+    return {'user': username, 'results': results, 'id': survey_id, 'survey_raw': survey_response, 'real_results': real_results}
