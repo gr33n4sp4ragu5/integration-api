e43bfdd1df4d8dfd981c800b7837a93ebc9eb34e
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-05-29 19:05:38 +0200

    style(#7): corrected style and changed wildcard for specific imports from db connector

--- colintmet_api/views/physiological_data.py
+++ colintmet_api/views/physiological_data.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework import status
-from colintmet_api.db_connector import *
+from colintmet_api.db_connector import (
+    establish_db_connection, insert_physiological_data)
 from colintmet_api.authentication import ColintmetTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 import logging
@@ -10,6 +11,7 @@ import logging
 DATABASE_NAME = 'colintmet-db'
 DATABASE_URL = 'some-mongo'
 DATABASE_PORT = 27017
+
 
 class PostPhysiologicalData(APIView):
     authentication_classes = (ColintmetTokenAuthentication,)
@@ -19,9 +21,16 @@ class PostPhysiologicalData(APIView):
         data = request.data
         physiological_data = data
         try:
-            db_connection = establish_db_connection(DATABASE_URL, DATABASE_PORT, DATABASE_NAME)
-            insert_physiological_data(db_connection, physiological_data, request.user.id)
-            return Response({"status": "Successfully added physiological data"}, status = status.HTTP_201_CREATED)
+            db_connection = establish_db_connection(
+                DATABASE_URL, DATABASE_PORT, DATABASE_NAME)
+            insert_physiological_data(
+                db_connection, physiological_data, request.user.id)
+            return Response(
+                {"status": "Successfully added physiological data"},
+                status=status.HTTP_201_CREATED)
         except Exception as error:
-            logging.error("Error while adding physiological data. Error is \n %s", error)
-            return Response({"status": "Something went wrong"}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+            logging.error(
+                "Error while adding physiological data. Error is \n %s", error)
+            return Response(
+                {"status": "Something went wrong"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
