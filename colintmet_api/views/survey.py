7228e69138a8989982f81cbb36a6c8c0c28fb871
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-05-29 19:14:08 +0200

    style(#7): fixed style in survey.py

--- colintmet_api/views/survey.py
+++ colintmet_api/views/survey.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework import status
-from colintmet_api.db_connector import *
+from colintmet_api.db_connector import (
+    establish_db_connection, insert_survey_response
+)
 from colintmet_api.authentication import ColintmetTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 import logging
@@ -10,6 +12,7 @@ import logging
 DATABASE_NAME = 'colintmet-db'
 DATABASE_URL = 'some-mongo'
 DATABASE_PORT = 27017
+
 
 class PostSurveyAnswer(APIView):
     authentication_classes = (ColintmetTokenAuthentication,)
@@ -19,9 +22,16 @@ class PostSurveyAnswer(APIView):
         data = request.data
         survey_response = data
         try:
-            db_connection = establish_db_connection(DATABASE_URL, DATABASE_PORT, DATABASE_NAME)
-            insert_survey_response(db_connection, survey_response, request.user.id)
-            return Response({"status": "Successfully added survey answer"}, status = status.HTTP_201_CREATED)
+            db_connection = establish_db_connection(
+                DATABASE_URL, DATABASE_PORT, DATABASE_NAME)
+            insert_survey_response(
+                db_connection, survey_response, request.user.id)
+            return Response(
+                {"status": "Successfully added survey answer"},
+                status=status.HTTP_201_CREATED)
         except Exception as error:
-            logging.error("Error while adding survey response. Error is \n %s", error)
-            return Response({"status": "Something went wrong"}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+            logging.error(
+                "Error while adding survey response. Error is \n %s", error)
+            return Response(
+                {"status": "Something went wrong"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
